f5e1570691160a1a721013a286a76ef4ccb4bc59
CharlesKinx <664410795@qq.com>
2020-12-17 22:54:28 +0800

    提示框

--- src/main/java/com/example/database/controller/RegisterController.java
+++ src/main/java/com/example/database/controller/RegisterController.java
@@ -1,6 +1,7 @@
 package com.example.database.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,8 +22,35 @@ public class RegisterController {
             @RequestParam(value = "account",required=false) String account,
             @RequestParam(value = "name",required=false) String name,
             @RequestParam(value = "email",required=false) String email,
-            @RequestParam(value = "password",required=false) String password
+            @RequestParam(value = "password",required=false) String password,
+            Model model
     ){
+
+        model.addAttribute("account",account);
+        model.addAttribute("name",name);
+        model.addAttribute("email",email);
+        model.addAttribute("password",password);
+
+        if(account == null || account ==""){
+            model.addAttribute("error", "账号不能为空！");
+            return "register";
+        }
+
+        if(name == null || name ==""){
+            model.addAttribute("error", "姓名不能为空！");
+            return "register";
+        }
+        if(email == null || email ==""){
+            model.addAttribute("error", "邮箱不能为空！");
+            return "register";
+        }
+
+        if(password == null || password ==""){
+            model.addAttribute("error", "密码不能为空！");
+            return "register";
+        }
+
+
 
         System.out.println(account+name+email+password);
         return "redirect:/";
